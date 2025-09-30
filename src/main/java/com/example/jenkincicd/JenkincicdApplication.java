67b7ce56d7243811bb635a3f6ffd1f19739823c4
sonalisawantfullstack <sonalisawantfullstack@gmail.com>
2025-09-30 09:38:48 -0500

    first commit

---
+++ src/main/java/com/example/jenkincicd/JenkincicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkincicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkincicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkincicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkincicd/JenkincicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkincicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkincicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
